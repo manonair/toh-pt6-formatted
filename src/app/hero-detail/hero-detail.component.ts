0fd1f1a22947c549273af3ae240f68e54fefbf04
manonair <manonair20@gmail.com>
2019-02-14 21:45:56 -0600

    chnages

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -25,7 +25,7 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id)
+    this.heroService.getReservation(id)
       .subscribe(hero => this.hero = hero);
   }
 
